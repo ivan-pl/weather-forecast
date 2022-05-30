6753a3274ba7a167728b7b5a9b8b5558658b6b00
ivan-pl <pleskaiv@gmail.com>
2022-05-30 17:29:42 +0300

    build: setup eslint+jest and refactor code

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,24 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    "jest/globals": true,
   },
-  extends: [
-    'airbnb-base',
-    'prettier'
-  ],
+  extends: ["airbnb-base", "prettier"],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
+    "max-len": [
+      "error",
+      { ignoreComments: true, ignorePattern: "^\\s*it.*\\{$" },
+    ],
+    "import/prefer-default-export": "off",
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error",
   },
+  plugins: ["jest"],
 };
