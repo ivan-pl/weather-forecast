4bbd68733256b4eb1c77512e9c9c2209ce647a14
ivan-pl <pleskaiv@gmail.com>
2022-05-31 12:25:29 +0300

    refactor: run prettier

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,14 @@
 module.exports = (api) => ({
-    presets: [
-      [
-        "@babel/preset-env",
-        {
-          useBuiltIns: "entry",
-          // caller.target will be the same as the target option from webpack
-          targets: api.caller((caller) => caller && caller.target === "node")
-            ? { node: "current" }
-            : { chrome: "58", ie: "11" },
-        },
-      ],
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "entry",
+        // caller.target will be the same as the target option from webpack
+        targets: api.caller((caller) => caller && caller.target === "node")
+          ? { node: "current" }
+          : { chrome: "58", ie: "11" },
+      },
     ],
-  });
+  ],
+});
