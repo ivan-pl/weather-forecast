8d16e7c4ed1e7335dc13bf93b259db3573bc8e64
ivan-pl <pleskaiv@gmail.com>
2022-10-26 15:55:04 +0300

    feat: add basic template

--- src/scripts/template.test.ts
+++ src/scripts/template.test.ts
@@ -1,7 +1,30 @@
 import template from "./template";
 
 describe("template", () => {
-  it("is a function", () => {
-    expect(template).toBeInstanceOf(Function);
+  const data = {
+    NAME: "Bob",
+    AGE: "18",
+    TEAM: "Core",
+    items: [
+      { A: 1, B: 2 },
+      { A: 11, B: 22 },
+      { A: 111, B: 222 },
+    ],
+  };
+
+  describe("basic data placing", () => {
+    it("puts data into placeholders", () => {
+      expect(template("Hi, {{NAME}}", data)).toBe("Hi, Bob");
+    });
+
+    it("puts empty string into placeholders in no data provided", () => {
+      expect(template("Hi, {{NAME}} {{SURNAME}}", data)).toBe("Hi, Bob ");
+    });
+
+    it("replaces all placeholders", () => {
+      expect(template("Hi, {{NAME}}. My name is {{NAME}} too", data)).toBe(
+        "Hi, Bob. My name is Bob too"
+      );
+    });
   });
 });
--- src/scripts/template.ts
+++ src/scripts/template.ts
@@ -1,4 +1,12 @@
-// eslint-disable-next-line
-export default function template(tpl: string, options: any): string {
-  return "";
+export default function template(
+  tpl: string,
+  data: { [key: string]: any }
+): string {
+  const result = tpl.replace(/\{\{(\w+)\}\}/g, (match, propName) => {
+    if (propName in data) {
+      return data[propName];
+    }
+    return "";
+  });
+  return result;
 }
