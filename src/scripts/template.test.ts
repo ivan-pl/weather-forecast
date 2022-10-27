929a6b86c6d942f9b54ccfd0245d4c6d420081f6
ivan-pl <pleskaiv@gmail.com>
2022-10-27 12:46:17 +0300

    feat: add template

--- src/scripts/template.test.ts
+++ src/scripts/template.test.ts
@@ -27,4 +27,50 @@ describe("template", () => {
       );
     });
   });
+
+  describe("loops", () => {
+    it("puts values from list elements inside loop", () => {
+      expect(
+        template("{{NAME}}{{for items}}{{NAME}},{{endfor}}", {
+          NAME: "0 ",
+          items: [{ NAME: "1" }, { NAME: "2" }],
+        })
+      ).toBe("0 1,2,");
+    });
+
+    it("handles basic loops", () => {
+      expect(template("{{for items}}{{A}},{{endfor}}", data)).toBe("1,11,111,");
+    });
+  });
+
+  describe("if", () => {
+    it("handles basic if", () => {
+      expect(
+        template("{{if FLAG}},{{NAME}},{{endif}}", { NAME: "A", FLAG: true })
+      ).toBe(",A,");
+      expect(
+        template("{{if FLAG}},{{NAME}},{{endif}}", { NAME: "A", FLAG: false })
+      ).toBe("");
+      expect(
+        template("{{if FLAG}},{{NAME}},{{endif}}s", { NAME: "A", FLAG: false })
+      ).toBe("s");
+      expect(
+        template("{{if FLAG}},{{NAME}},{{endif}}s", { NAME: "A", FLAG: true })
+      ).toBe(",A,s");
+    });
+
+    it("handles if-else", () => {
+      const data = {
+        NAME: "A",
+        SURNAME: "B",
+        FLAG: true,
+      };
+      expect(
+        template("{{if FLAG}},{{NAME}},{{else}}.{{SURNAME}}.{{endif}}!", data)
+      ).toBe(",A,!");
+      expect(
+        template("{{if FLAG}},{{NAME}},{{else}}.{{SURNAME}}.{{endif}}!", {...data, FLAG: false})
+      ).toBe(".B.!");
+    });
+  });
 });
--- src/scripts/template.ts
+++ src/scripts/template.ts
@@ -2,11 +2,46 @@ export default function template(
   tpl: string,
   data: { [key: string]: any }
 ): string {
-  const result = tpl.replace(/\{\{(\w+)\}\}/g, (match, propName) => {
+  let newTpl = "";
+  newTpl = loopReplace(tpl, data);
+  newTpl = ifReplace(newTpl, data);
+  newTpl = variableReplace(newTpl, data);
+
+  return newTpl;
+}
+
+function variableReplace(tpl: string, data: { [key: string]: any }) {
+  return tpl.replace(/\{\{(\w+)\}\}/g, (_, propName) => {
     if (propName in data) {
       return data[propName];
     }
     return "";
   });
-  return result;
+}
+
+function loopReplace(tpl: string, data: { [key: string]: any }) {
+  return tpl.replace(
+    /\{\{for (\w+)\}\}(.+?)\{\{endfor\}\}/g,
+    (_, propName, subTemplate) => {
+      if (Array.isArray(data[propName])) {
+        return data[propName].reduce(
+          (prev: string, item: { [key: string]: any }) =>
+            prev + template(subTemplate, item),
+          ""
+        );
+      }
+      return "";
+    }
+  );
+}
+
+function ifReplace(tpl: string, data: { [key: string]: any }) {
+  return tpl.replace(
+    /\{\{if (\w+)\}\}(.+?)(?:\{\{else\}\}(.+?))?\{\{endif\}\}/g,
+    (_, flagName, subTemplateTrue, subTemplateFalse) => {
+      return data[flagName]
+        ? template(subTemplateTrue, data)
+        : template(subTemplateFalse || "", data);
+    }
+  );
 }
